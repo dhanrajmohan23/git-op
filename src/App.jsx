e146370645392f34db1adbea52c1300b00c417ff
dhanrajMohan <dhanrajmohan23@gmail.com>
2023-05-23 12:16:39 +0530

    Count removed

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,6 @@ function App() {
         </a>
       </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
